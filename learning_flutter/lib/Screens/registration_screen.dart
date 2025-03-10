9e62a83768c61f4f0a8e3cb9686750ac222e0b75
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-10 16:36:58 +0500

    Making a statefull widget named registration_screen

--- learning_flutter/lib/Screens/registration_screen.dart
+++ learning_flutter/lib/Screens/registration_screen.dart
@@ -10,6 +10,67 @@ class Registration extends StatefulWidget {
 class _RegistrationState extends State<Registration> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Registration Screen"));
+    double screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 120),
+            Text(
+              "Registration Screen",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w900),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 65),
+              child: SizedBox(
+                width: screenWidth * 0.78,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                    suffixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 25),
+              child: SizedBox(
+                width: screenWidth * 0.78,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    suffixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 1),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 1),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(2),
+                      borderSide: BorderSide(color: Colors.black, width: 2),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
