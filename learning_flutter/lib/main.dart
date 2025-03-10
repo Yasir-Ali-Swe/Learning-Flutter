0b85335e9ef65da8eeae00afd433c1785c140f52
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-10 16:11:38 +0500

    Making a statefull widget named Login_Screen

--- learning_flutter/lib/main.dart
+++ learning_flutter/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:learning_flutter/login_screen.dart';
+import 'package:learning_flutter/Screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
