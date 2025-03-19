7eb380156e76bef45cfe766ff291695a834096e5
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-20 00:29:55 +0500

    Lab 3 how to add images adn assets in flutter project

---
+++ lab_3/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter Lab 1",
+          style: TextStyle(
+            color: Colors.yellow,
+            fontSize: 26,
+            fontWeight: FontWeight.w900,
+          ),
+        ),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 40),
+            Text(
+              "Yasir Ali",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 26,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+            SizedBox(height: 80),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.network(
+                    "https://static.vecteezy.com/system/resources/thumbnails/000/439/863/small/Basic_Ui__28186_29.jpg",
+                  ),
+                ),
+                SizedBox(width: 30),
+                SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: Image.asset("user.png"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
