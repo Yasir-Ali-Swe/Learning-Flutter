65d72a09c59d2d3938ef992fb74796c039466488
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2025-03-19 10:37:03 +0500

    Lab 6 Toggle Theme

--- lab_6/lib/main.dart
+++ lab_6/lib/main.dart
@@ -4,38 +4,78 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkTheme = true;
+
+  void toggleTheme() {
+    setState(() {
+      isDarkTheme = !isDarkTheme;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: isDarkTheme ? Colors.white : Colors.green,
+          ),
+        ),
       ),
-      home: mainPage(),
+      home: MainPage(toggleTheme: toggleTheme),
     );
   }
 }
 
-class mainPage extends StatelessWidget {
-  const mainPage({super.key});
+class MainPage extends StatelessWidget {
+  final VoidCallback toggleTheme;
+  const MainPage({super.key, required this.toggleTheme});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Lab 6",
+        title: const Text(
+          "Toggle Theme",
           style: TextStyle(
             fontSize: 26,
             fontWeight: FontWeight.w900,
             color: Colors.white,
           ),
         ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Theme.of(context).brightness == Brightness.dark
+                  ? Icons.toggle_on
+                  : Icons.toggle_off,
+              size: 30,
+            ),
+            onPressed: toggleTheme,
+          ),
+        ],
         centerTitle: true,
         backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 60),
+            Text("Yasir Ali", style: Theme.of(context).textTheme.bodyLarge),
+          ],
+        ),
       ),
     );
   }
